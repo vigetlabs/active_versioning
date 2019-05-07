c948d295c5eacbf3f37f5e13a9fd2989c3a43db1
David Eisinger <david.eisinger@gmail.com>
2019-05-07 15:24:07 -0400

    Reformat specs

--- spec/lib/active_versioning/nested_attributes_spec.rb
+++ spec/lib/active_versioning/nested_attributes_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe ActiveVersioning::Model::Versioned do
+RSpec.describe "assigning nested attributes" do
   let!(:post) do
     ActiveVersioning::Test::Post.create(
       title: 'So Post',
@@ -10,52 +10,54 @@ RSpec.describe ActiveVersioning::Model::Versioned do
 
   let!(:committed_at) { Time.utc(2014, 11, 26, 12, 42, 35) }
 
-  describe "assigning nested attributes" do
-    let(:subject) { post.current_draft }
+  let(:subject) { post.current_draft }
 
-    it "creates a new belongs-to relationship" do
-      expect {
-        subject.assign_attributes(author_attributes: { name: "Steve" })
-        subject.save
-      }.not_to change {
-        ActiveVersioning::Test::User.count
-      }
-
-      p = ActiveVersioning::Test::Post.find(post.id).current_draft
-
-      expect { p.commit }.to change { ActiveVersioning::Test::User.count }.by(1)
-    end
-
-    it "updates an existing belongs-to relationship" do
+  it "creates a new belongs-to relationship" do
+    expect {
       subject.assign_attributes(author_attributes: { name: "Steve" })
       subject.save
-      subject.commit
+    }.not_to change {
+      ActiveVersioning::Test::User.count
+    }
 
-      p = ActiveVersioning::Test::Post.find(post.id).current_draft
+    p = ActiveVersioning::Test::Post.find(post.id).current_draft
 
-      p.assign_attributes(author_attributes: { id: p.author.id, name: "Steeve" })
-      p.save
-
-      p = ActiveVersioning::Test::Post.find(post.id).current_draft
-
-      expect {
-        p.commit
-      }.to change {
-        ActiveVersioning::Test::User.first.name
-      }.from("Steve").to("Steeve")
-    end
-
-    it "creates a new has-many relationship" do
-      expect {
-        subject.assign_attributes(comments_attributes: [{ body: "First" }, { body: "Second" }])
-        subject.save
-      }.not_to change {
-        ActiveVersioning::Test::Comment.count
-      }
-
-      p = ActiveVersioning::Test::Post.find(post.id).current_draft
-
-      expect { p.commit }.to change { ActiveVersioning::Test::Comment.count }.by(2)
-    end
+    expect { p.commit }.to change { ActiveVersioning::Test::User.count }.by(1)
   end
+
+  it "updates an existing belongs-to relationship" do
+    subject.assign_attributes(author_attributes: { name: "Steve" })
+    subject.save
+    subject.commit
+
+    p = ActiveVersioning::Test::Post.find(post.id).current_draft
+
+    p.assign_attributes(author_attributes: { id: p.author.id, name: "Steeve" })
+    p.save
+
+    p = ActiveVersioning::Test::Post.find(post.id).current_draft
+
+    expect {
+      p.commit
+    }.to change {
+      ActiveVersioning::Test::User.first.name
+    }.from("Steve").to("Steeve")
+  end
+
+  it "creates a new has-many relationship" do
+    expect {
+      subject.assign_attributes(comments_attributes: [{ body: "First" }, { body: "Second" }])
+      subject.save
+    }.not_to change {
+      ActiveVersioning::Test::Comment.count
+    }
+
+    p = ActiveVersioning::Test::Post.find(post.id).current_draft
+
+    expect { p.commit }.to change { ActiveVersioning::Test::Comment.count }.by(2)
+  end
+
+  # updates existing has-many relationships
+  # creates and updates has-many relationships simultaneously
+  # deletes has-many relationships
 end
